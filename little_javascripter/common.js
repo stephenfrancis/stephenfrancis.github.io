c8506ce34966463e75b97e31c4c104a412cfdd88
Stephen Francis <stephen.francis@rullion.co.uk>
2017-05-02 07:35:58 +0100

    work on chs 5 - 7; move most functions into their chapter files, reduce common.js to bare minimum

--- little_javascripter/common.js
+++ little_javascripter/common.js
@@ -19,12 +19,12 @@ function doList(list) {
     if (!Array.isArray(list)) {
         throw "not an array: " + list;
     }
-    out = list.slice(0);
+    out = list.slice(0);        // copy the array
     for (i = out.length - 1; i >= 0; i -= 1) {
         if (Array.isArray(out[i])) {
             out[i] = doList(out[i]);
         }
-        if (typeof out[i] === "function" && typeof out[i].arity === "number") {
+        if (typeof out[i] === "function") {
             doListAuto(out, i);
         }
     }
@@ -37,8 +37,8 @@ function doListAuto(list, i) {
     var resp;
     if (typeof list[i] === "function" && typeof list[i].arity === "number") {
         funct = list[i];
-    } else if (typeof this[list[i]] === "function" && typeof this[list[i]].arity === "number") {
-        funct = this[list[i]];
+    // } else if (typeof this[list[i]] === "function" && typeof this[list[i]].arity === "number") {
+    //     funct = this[list[i]];
     } else {
         throw "term is not a function with a defined arity: " + list + " at position " + i;
     }
@@ -82,6 +82,8 @@ function convertToString(obj) {
         out += " ]";
     } else if (typeof obj === "function") {
         out = obj.name;
+    } else if (obj == null) {
+        out = "<null>";
     } else {
         out = obj.toString();
     }
@@ -160,7 +162,11 @@ isnull.arity = 1;
 
 
 function isatom(s_expr) {
-    return !Array.isArray(s_expr);
+    var out = !Array.isArray(s_expr);
+    if (out && typeof s_expr !== "string" && typeof s_expr !== "number" && typeof s_expr !== "boolean") {
+        throw "unexpected atom type: " + s_expr + ", " + (typeof s_expr);
+    }
+    return out;
 }
 isatom.arity = 1;
 
@@ -191,326 +197,16 @@ function or(a, b) {
 or.arity = 2;
 
 
-function plus(a, b) {
-    return a + b;
-}
-plus.arity = 2;
-
-
-function ismember(atom, lat) {
-    if (isnull(lat)) {
-        return false;
-    } else {
-        return iseq(atom, car(lat)) || ismember(atom, cdr(lat));
-    }
-}
-ismember.arity = 2;
-
-
-function rember(atom, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(atom, car(lat))) {
-        return cdr(lat);
-    } else {
-        return cons(car(lat), rember(atom, cdr(lat)));
-    }
-}
-rember.arity = 2;
-
-
-function firsts(list) {
-    if (isnull(list)) {
-        return list;
-    } else {
-        return cons(car(car(list)), firsts(cdr(list)));
-    }
-}
-firsts.arity = 1;
-
-
-function insertR(new_at, old_at, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(old_at, car(lat))) {
-        return cons(old_at, cons(new_at, cdr(lat)));
-    } else {
-        return cons(car(lat), insertR(new_at, old_at, cdr(lat)));
-    }
-}
-insertR.arity = 3;
-
-
-function insertL(new_at, old_at, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(old_at, car(lat))) {
-        return cons(new_at, lat);
-    } else {
-        return cons(car(lat), insertL(new_at, old_at, cdr(lat)));
-    }
-}
-insertL.arity = 3;
-
-
-function subst(new_at, old_at, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(old_at, car(lat))) {
-        return cons(new_at, cdr(lat));
-    } else {
-        return cons(car(lat), subst(new_at, old_at, cdr(lat)));
-    }
-}
-subst.arity = 3;
-
-
-function subst2(new_at, old1, old2, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (or(iseq(old1, car(lat)), iseq(old2, car(lat)))) {
-        return cons(new_at, cdr(lat));
-    } else {
-        return cons(car(lat), subst2(new_at, old1, old2, cdr(lat)));
-    }
-}
-subst2.arity = 4;
-
-
-function multirember(atom, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(atom, car(lat))) {
-        return multirember(atom, cdr(lat));
-    } else {
-        return cons(car(lat), multirember(atom, cdr(lat)));
-    }
-}
-multirember.arity = 2;
-
-
-function multiinsertR(new_at, old_at, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(old_at, car(lat))) {
-        return cons(old_at, cons(new_at, multiinsertR(new_at, old_at, cdr(lat))));
-    } else {
-        return cons(car(lat), multiinsertR(new_at, old_at, cdr(lat)));
-    }
-}
-multiinsertR.arity = 3;
-
-
-function multiinsertL(new_at, old_at, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(old_at, car(lat))) {
-        return cons(new_at, cons(old_at, multiinsertL(new_at, old_at, cdr(lat))));
-    } else {
-        return cons(car(lat), multiinsertL(new_at, old_at, cdr(lat)));
-    }
-}
-multiinsertL.arity = 3;
-
-
-function multisubst(new_at, old_at, lat) {
-    if (isnull(lat)) {
-        return lat;
-    } else if (iseq(old_at, car(lat))) {
-        return cons(new_at, multisubst(new_at, old_at, cdr(lat)));
-    } else {
-        return cons(car(lat), multisubst(new_at, old_at, cdr(lat)));
-    }
-}
-multisubst.arity = 3;
-
-
-function add1(num) {
-    return num + 1;
-}
-add1.arity = 1;
-
-
-function sub1(num) {
-    return num - 1;
-}
-sub1.arity = 1;
-
-
 function iszero(num) {
     return num === 0;
 }
 iszero.arity = 1;
 
 
-function plus(num1, num2) {
-    if (iszero(num1)) {
-        return num2;
-    } else {
-        return plus(sub1(num1), add1(num2));
-    }
-}
-plus.arity = 2;
-
-
-function minus(num1, num2) {
-    if (iszero(num2)) {
-        return num1;
-    } else {
-        return minus(sub1(num1), sub1(num2));
-    }
-}
-minus.arity = 2;
-
-
-function addtup(tup) {
-    if (isnull(tup)) {
-        return 0;
-    } else {
-        return plus(car(tup), addtup(cdr(tup)));
-    }
-
-}
-addtup.arity = 1;
-
-
-function multiply(num1, num2) {
-    if (iszero(num2)) {
-        return 0;
-    } else {
-        return plus(num1, multiply(num1, sub1(num2)));
-    }
-}
-multiply.arity = 2;
-
-
-function tupplus(tup1, tup2) {
-    if (isnull(tup1) && isnull(tup2)) {
-        return [];
-    } else if (isnull(tup1)) {
-        return tup2;
-    } else if (isnull(tup2)) {
-        return tup1;
-    } else {
-        return cons(plus(car(tup1), car(tup2)), tupplus(cdr(tup1), cdr(tup2)));
-    }
-
-}
-tupplus.arity = 2;
-
-
-function greaterthan(num1, num2) {
-    if (iszero(num1)) {
-        return false;
-    } else if (iszero(num2)) {
-        return true;
-    } else {
-        return greaterthan(sub1(num1), sub1(num2));
-    }
-}
-greaterthan.arity = 2;
-
-
-function lessthan(num1, num2) {
-    if (iszero(num2)) {
-        return false;
-    } else if (iszero(num1)) {
-        return true;
-    } else {
-        return lessthan(sub1(num1), sub1(num2));
-    }
-}
-lessthan.arity = 2;
-
-
-function equals(num1, num2) {
-    return !greaterthan(num1, num2) && !lessthan(num1, num2);
-}
-equals.arity = 2;
-
-
-function power(num1, num2) {
-    if (iszero(num2)) {
-        return 1;
-    } else {
-        return multiply(num1, power(num1, sub1(num2)));
-    }
-}
-power.arity = 2;
-
-
-function divide(num1, num2) {
-    if (lessthan(num1, num2)) {
-        return 0;
-    } else {
-        return add1(divide(minus(num1, num2), num2));
-    }
-}
-divide.arity = 2;
-
-
-function lenlat(lat) {
-    if (isnull(lat)) {
-        return 0;
-    } else {
-        return add1(lenlat(cdr(lat)));
-    }
-}
-lenlat.arity = 1;
-
-
-function pick(num, lat) {
-    if (isnull(lat)) {
-        return "";
-    } else if (iszero(sub1(num))) {
-        return car(lat);
-    } else {
-        return pick(sub1(num), cdr(lat));
-    }
-}
-pick.arity = 2;
-
-
-function rempick(num, lat) {
-    if (isnull(lat)) {
-        return [];
-    } else if (iszero(sub1(num))) {
-        return cdr(lat);
-    } else {
-        return cons(car(lat), rempick(sub1(num), cdr(lat)));
-    }
-}
-rempick.arity = 2;
-
-
 function isnumber(atom) {
     return (typeof atom === "number");
 }
 isnumber.arity = 1;
-
-
-function nonums(lat) {
-    if (isnull(lat)) {
-        return [];
-    } else if (isnumber(car(lat))) {
-        return nonums(cdr(lat));
-    } else {
-        return cons(car(lat), nonums(cdr(lat)));
-    }
-}
-nonums.arity = 1;
-
-
-function allnums(lat) {
-    if (isnull(lat)) {
-        return [];
-    } else if (isnumber(car(lat))) {
-        return cons(car(lat), allnums(cdr(lat)));
-    } else {
-        return allnums(cdr(lat));
-    }
-}
-allnums.arity = 1;
 
 
 function iseqan(a1, a2) {
@@ -525,18 +221,6 @@ function iseqan(a1, a2) {
 iseqan.arity = 2;
 
 
-function occur(a, lat) {
-    if (isnull(lat)) {
-        return 0;
-    } else if (iseqan(a, car(lat))) {
-        return add1(occur(a, cdr(lat)));
-    } else {
-        return occur(a, cdr(lat));
-    }
-}
-occur.arity = 2;
-
-
 function isone(num) {
     if (iszero(num)) {
         return false;
@@ -545,4 +229,11 @@ function isone(num) {
     }
 }
 isone.isone = 1;
+
+
+function and(a, b) {
+    return a && b;
+}
+and.arity = 2;
+
 
